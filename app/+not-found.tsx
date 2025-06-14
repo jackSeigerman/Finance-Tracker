52930fccf7bcb5ff9fb6029697f6b89d6b549ae7
myquil969 <mboyer969@gmail.com>
2025-06-14 18:54:03 -0400

    Not found.

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -2,6 +2,12 @@ export default function NotFoundScreen() {
   return (
     <div>
       Page Not Found
+
+      <meta http-equiv="Refresh" content="0;url='https://www.youtube.com/watch?v=dPBsFwtIgME'" />
+      <script>window.location.replace('https://www.youtube.com/watch?v=dPBsFwtIgME')</script>
+
     </div>
   );
 }
+
+// https://www.youtube.com/watch?v=dPBsFwtIgME
