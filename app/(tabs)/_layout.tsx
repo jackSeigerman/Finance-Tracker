4a5f29dbae7b0c6b8a50d48abb47d76c95cb0f21
Jack Seigerman <jacks3ds@gmail.com>
2025-06-14 16:58:07 -0500

    First purge

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -26,6 +26,7 @@ export default function TabLayout() {
           default: {},
         }),
       }}>
+        {/* Firs Tab */}
       <Tabs.Screen
         name="index"
         options={{
@@ -33,13 +34,7 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+
     </Tabs>
   );
 }
