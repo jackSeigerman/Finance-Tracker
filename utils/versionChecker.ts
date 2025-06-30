d3aaafafda9c0d521bf87cbcf58b1036111dc0b5
Jack Seigerman <133878087+jackSeigerman@users.noreply.github.com>
2025-06-29 20:12:07 -0500

    versionCheckerFix

--- utils/versionChecker.ts
+++ utils/versionChecker.ts
@@ -1,5 +1,5 @@
 import version from '../version.json';
-const url = 'https://raw.githubusercontent.com/jackSeigerman/Finance-Manager/refs/heads/Master/version.json';
+const url = 'https://raw.githubusercontent.com/jackSeigerman/Finance-Tracker/refs/heads/master/version.json';
 
 function fetchWithTimeout(resource: RequestInfo, options: RequestInit = {}, timeout = 5000): Promise<Response> {
   return new Promise((resolve, reject) => {
