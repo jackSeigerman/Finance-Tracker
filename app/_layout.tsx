1e951df66c5223484d4288b5881e3c9e27be717e
Jack Seigerman <133878087+jackSeigerman@users.noreply.github.com>
2025-07-01 23:15:50 -0500

    remove version checker

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,6 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-import { checkTagStatus } from '../utils/versionChecker';
 import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, Platform, TouchableOpacity, Alert } from 'react-native';
 
@@ -12,31 +11,7 @@ export default function RootLayout() {
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
-  useEffect(() => {
-    async function checkForUpdates() {
-      const status = await checkTagStatus();
-      if (status === 1) {
-        if (Platform.OS === 'web') {
-          setErrorMessage('A new version of The Finance Tracker is available.');
-        }
-        else {
-          Alert.alert('A new version of The Finance Tracker is available. Please update the app.');
-        }
-        console.log(status);
-      } else if (status === 2) {
-        if (Platform.OS === 'web') {
-          setErrorMessage('Error checking for updates. Check your internet connection.');
-        } else {
-          Alert.alert('Error checking for updates. Check your internet connection and try again later.');
-        }
-        console.log(status);
-      }
-    }
-
-    checkForUpdates();
-  }, []);
 
   if (!loaded) {
     return null;
@@ -45,14 +20,8 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={'dark' === 'dark' ? DarkTheme : DefaultTheme}>
       <View style={{ flex: 1 }}>
-        {errorMessage && (
-          <View style={styles.errorBox}>
-            <Text style={styles.errorText}>{errorMessage}</Text>
-            <TouchableOpacity onPress={() => setErrorMessage(null)}>
-              <Text style={styles.dismiss}>✕</Text>
-            </TouchableOpacity>
-          </View>
-        )}
+       
+
         <Stack>
           <Stack.Screen name="index" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
@@ -63,24 +32,4 @@ export default function RootLayout() {
   );
 }
 
-const styles = StyleSheet.create({
-  errorBox: {
-    backgroundColor: '#FF5A5F',
-    padding: 10,
-    paddingTop: Platform.OS === 'web' ? 20 : 50,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    zIndex: 999,
-  },
-  errorText: {
-    color: '#fff',
-    flex: 1,
-    fontWeight: 'bold',
-  },
-  dismiss: {
-    color: '#fff',
-    fontSize: 18,
-    marginLeft: 10,
-  },
-});
+
